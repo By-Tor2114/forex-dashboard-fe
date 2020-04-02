77c92432a93ca4f774406e0ef2ca9907f0b795f5
By-Tor2114 <by_tor2114_uk@yahoo.com>
2020-04-02 16:37:54 +0100

    Adds basic media query to navbar

--- src/containers/TradeHistory/TradeHistory.jsx
+++ src/containers/TradeHistory/TradeHistory.jsx
@@ -8,11 +8,11 @@ const { getTrades } = require('../../utils/get-trades');
 const TradeHistory = ({ token, user }) => {
   const [trades, setTrades] = useState([]);
   const [balance, setBalance] = useState(0);
+  const [showTrades, setShowTrades] = useState(true);
 
   useEffect(() => {
     const fetchTrades = async () => {
       const { trades } = await getTrades(token);
-      console.log(trades[0].dateOpened);
 
       setTrades(trades);
       balanceCalc(trades);
@@ -42,21 +42,18 @@ const TradeHistory = ({ token, user }) => {
       .join('/');
   };
 
-  return (
-    <div className="TradeHistory">
-      <div className="history-head">
-        <h2 className="m-1">
-          Trade Hi<span className="span-green">$</span>tory
-        </h2>
-        <Button styling="button-add m-1">Add Trade</Button>
-      </div>
+  const listToggler = event => {
+    event.preventDefault();
+    setShowTrades(!showTrades);
+  };
 
-      <div className="history-balance ml-1">
-        <p>Running Balance: {balance} </p>
-      </div>
+  let list;
+
+  if (showTrades) {
+    list = (
       <ul>
         {trades.map((trade, index) => (
-          <li key={index} className="test">
+          <li key={index} className="trade-list">
             <div className={trade.outcome.toLowerCase()}>
               <p>{trade.currencyPair}</p>
               <p>{trade.outcome} </p>
@@ -77,6 +74,28 @@ const TradeHistory = ({ token, user }) => {
           </li>
         ))}
       </ul>
+    );
+  } else {
+    list = null;
+  }
+
+  return (
+    <div className="TradeHistory">
+      <div className="history-head">
+        <h2 className="m-1">
+          Trade Hi<span className="span-green">$</span>tory
+        </h2>
+        <Button styling="button-add m-1">Add Trade</Button>
+      </div>
+
+      <div className="history-balance ml-1 mr-1">
+        <p>Running Balance: {balance} </p>
+        <Button toggle={listToggler} styling="hide-list">
+          {showTrades ? 'Hide' : 'Show'} Trade List
+        </Button>
+      </div>
+
+      {list}
     </div>
   );
 };
