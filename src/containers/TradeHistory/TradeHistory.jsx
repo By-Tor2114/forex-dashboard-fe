2c9103442ee1835d5b22740ef4fd2e93958141e0
By-Tor2114 <by_tor2114_uk@yahoo.com>
2020-04-02 15:51:54 +0100

    Completes basic styling of trade history list

--- src/containers/TradeHistory/TradeHistory.jsx
+++ src/containers/TradeHistory/TradeHistory.jsx
@@ -6,16 +6,41 @@ import './TradeHistory.css';
 const { getTrades } = require('../../utils/get-trades');
 
 const TradeHistory = ({ token, user }) => {
-  const [trades, setTrades] = useState([1, 2]);
+  const [trades, setTrades] = useState([]);
+  const [balance, setBalance] = useState(0);
 
   useEffect(() => {
     const fetchTrades = async () => {
       const { trades } = await getTrades(token);
+      console.log(trades[0].dateOpened);
 
       setTrades(trades);
+      balanceCalc(trades);
     };
     fetchTrades();
-  }, []);
+  }, [token]);
+
+  const balanceCalc = trades => {
+    let balance = user.accountBalance;
+
+    trades.forEach(trade => {
+      trade.outcome == 'winner'
+        ? (balance += trade.profitLoss)
+        : (balance -= trade.profitLoss);
+    });
+
+    setBalance(balance);
+  };
+
+  const dateFormatter = date => {
+    return date
+      .toString()
+      .replace(/-/g, '/')
+      .slice(2, 10)
+      .split('/')
+      .reverse()
+      .join('/');
+  };
 
   return (
     <div className="TradeHistory">
@@ -27,27 +52,27 @@ const TradeHistory = ({ token, user }) => {
       </div>
 
       <div className="history-balance ml-1">
-        <p>Account Balance: {user.accountBalance} </p>
+        <p>Running Balance: {balance} </p>
       </div>
       <ul>
         {trades.map((trade, index) => (
-          <li key={index} className={trade.outcome}>
-            <div className="m-05">
+          <li key={index} className="test">
+            <div className={trade.outcome.toLowerCase()}>
               <p>{trade.currencyPair}</p>
               <p>{trade.outcome} </p>
             </div>
 
-            <div className="m-05">
-              <p> Opened: {trade.dateOpened} </p>
-              <p> Closed: {trade.dateClosed} </p>
+            <div className={trade.outcome.toLowerCase()}>
+              <p> Opened: {dateFormatter(trade.dateOpened)} </p>
+              <p> Closed: {dateFormatter(trade.dateClosed)} </p>
             </div>
 
-            <div className="m-05">
+            <div className={trade.outcome.toLowerCase()}>
               <p>
-                {trade.outcome === 'winner' ? 'Profit' : 'Loss'}:{' '}
+                {trade.outcome === 'Winner' ? 'Profit' : 'Loss'}:{' '}
                 {trade.profitLoss || 'N/A'}
               </p>
-              <p>View Trade</p>
+              <p className="view-trade">View Trade</p>
             </div>
           </li>
         ))}
