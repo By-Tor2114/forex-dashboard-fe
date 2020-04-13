67b263d0073dfdbd8269ceb44e530cd928af032b
By-Tor2114 <by_tor2114_uk@yahoo.com>
2020-04-13 10:51:25 +0100

    Adds inital toggle for editing a pending trade

---
+++ src/components/AddEditPendingModal/AddEditPendingModal.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './AddEditPendingModal.css';
+
+const AddEditPendingModal = () => {
+  return (
+    <div className="AddEditPendingModal">
+      <h1>test</h1>
+    </div>
+  );
+};
+
+export default AddEditPendingModal;
--- src/containers/Pending/Pending.jsx
+++ src/containers/Pending/Pending.jsx
@@ -4,9 +4,33 @@ import './Pending.css';
 import Button from '../../components/Button/Button';
 import { getPending } from '../../utils/get-pending';
 import { dateFormatter } from '../../utils/helper-funcs';
+import AddEditPendingModal from '../../components/AddEditPendingModal/AddEditPendingModal';
 
 const Pending = ({ token }) => {
+  // Inital array of pending trades
   const [pendingTrades, setPendingTrades] = useState([]);
+
+  // Sets whether posting or patching a trade
+  const [method, setMethod] = useState('');
+
+  // Show view/edit trade modal and sets individual trade for modal
+  const [showViewPendingModal, setShowViewPendingModal] = useState(false);
+  const [singleTrade, setSingleTrade] = useState({});
+
+  // Show trade list, add trade modal and refreshes trade list
+  const [showTrades, setShowTrades] = useState(false);
+  const [showAddPendingModal, setShowAddPendingModal] = useState(false);
+  const [updateTrades, setUpdateTrades] = useState(false);
+
+  const addPendingModal = (event) => {};
+
+  const viewPendingModal = (event, trade) => {
+    event.preventDefault();
+    setMethod('PATCH');
+    setShowAddPendingModal(false);
+    setSingleTrade(trade);
+    setShowViewPendingModal(!showViewPendingModal);
+  };
 
   useEffect(() => {
     const fetchPendings = async () => {
@@ -39,11 +63,17 @@ const Pending = ({ token }) => {
               <p>Posted: {dateFormatter(trade.datePosted)}</p>
             </div>
             <div>
-              <p className="view-trade">View Trade</p>
+              <p
+                onClick={(event) => viewPendingModal(event, trade)}
+                className="view-trade"
+              >
+                View Trade
+              </p>
             </div>
           </li>
         ))}
       </ul>
+      {showViewPendingModal && <AddEditPendingModal />}
     </div>
   );
 };
